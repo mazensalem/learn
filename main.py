8891174a921bed75ccae60d19df568020e72ebbd
mazensalem <salemmazen27@gmail.com>
2020-11-08 19:13:41 +0200

    Solve the first 3 problems in leetcode

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("Wellcome")
+def solve(n):
+    nums = [0 for i in range(n + 1)]
+    nums[0] = 0
+    nums[1] = 1
+    i = 1
+    while True:
+        isstop = False
+        if 2 <= 2 * i and 2 * i <= n:
+            nums[2 * i] = nums[i]
+        else:
+            isstop = True
+        if 2 <= 2 * i + 1 and 2 * i + 1 <= n:
+            nums[2 * i + 1] = nums[i] + nums[i + 1]
+        else:
+            if isstop:
+                break
+        i += 1
+
+    return max(nums)
+
+
+def solve1(s):
+    ns = "".join(list(set(s)))
+    di = {}
+    result = 0
+    for char in ns:
+        delete = 0
+        vals = di.values()
+        while s.count(char) - delete in vals:
+            result += 1
+            delete += 1
+        else:
+            if s.count(char) - delete:
+                di[char] = s.count(char) - delete
+    return result
+
+def solve3(inventory, orders):
+    result = 0
+    while orders:
+        result += max(inventory)
+        inventory[inventory.index(max(inventory))] -= 1
+        orders -= 1
+    return result
+
+print(solve3([1000000000], 1000000000))
